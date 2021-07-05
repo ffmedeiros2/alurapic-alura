b02256337f89db78159fb41155b097a16a3acce2
Felipe Medeiros <ffmedeiros2@gmail.com>
2021-07-04 21:01:07 -0300

    add card

--- alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -1,10 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CardModule } from './../../shared/components/card/card.module';
 import { PhotoModule } from './../photo/photo.module';
 import { FilterByDescriptionPipe } from './filter-by-description.pipe';
 import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { PhotoListComponent } from './photo-list.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
@@ -13,6 +15,6 @@ import { CommonModule } from '@angular/common';
         PhotosComponent,
         FilterByDescriptionPipe,
     ],
-    imports: [CommonModule, PhotoModule],
+    imports: [CommonModule, PhotoModule, CardModule],
 })
 export class PhotoListModule {}
