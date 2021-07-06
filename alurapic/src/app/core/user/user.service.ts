f95fc268797036e9814b4070384d726bea34872f
Felipe Medeiros <ffmedeiros2@gmail.com>
2021-07-06 20:47:21 -0300

    add user service

---
+++ alurapic/src/app/core/user/user.service.ts
@@ -0,0 +1,31 @@
+import { User } from './user';
+import { Subject, Observable } from 'rxjs';
+import { TokenService } from './../token/token.service';
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserService {
+    private userSubject = new Subject<User>();
+
+    constructor(private tokenService: TokenService) {
+        this.tokenService.hasToken() && this.decodeAndNotify();
+    }
+
+    setToken(token: string) {
+        this.tokenService.setToken(token);
+        this.decodeAndNotify();
+    }
+
+    getUserName(): Observable<User> {
+        return this.userSubject.asObservable();
+    }
+
+    private decodeAndNotify() {
+        const token = this.tokenService.getToken() as string;
+        const user = jwt_decode(token) as User;
+        this.userSubject.next(user);
+    }
+}
