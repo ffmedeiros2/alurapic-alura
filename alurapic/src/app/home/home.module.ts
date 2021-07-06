0cae9037a435ae211f1be363609d0cf69cf5b7ab
Felipe Medeiros <ffmedeiros2@gmail.com>
2021-07-06 08:40:44 -0300

    add vmessage component

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SigninComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { VmessageModule } from './../shared/components/vmessage/vmessage.module';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
     declarations: [SigninComponent],
-    imports: [CommonModule, ReactiveFormsModule],
+    imports: [CommonModule, ReactiveFormsModule, VmessageModule],
 })
 export class HomeModule {}
