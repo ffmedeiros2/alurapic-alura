add1ece579c91fa48f56deaa53269050b17392c7
Felipe Medeiros <ffmedeiros2@gmail.com>
2021-07-15 21:17:27 -0300

    add request interceptor

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -1,11 +1,20 @@
+import { RequestInterceptor } from './auth/request.interceptor';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
     declarations: [HeaderComponent],
     imports: [CommonModule, RouterModule],
     exports: [HeaderComponent],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: RequestInterceptor,
+            multi: true,
+        },
+    ],
 })
 export class CoreModule {}
