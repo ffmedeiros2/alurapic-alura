c44386f2249890a70490ea7d6102125368f2d412
Felipe Medeiros <ffmedeiros2@gmail.com>
2021-07-15 21:41:32 -0300

    add footer component

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations: [HeaderComponent],
+    declarations: [HeaderComponent, FooterComponent],
     imports: [CommonModule, RouterModule],
-    exports: [HeaderComponent],
+    exports: [HeaderComponent, FooterComponent],
     providers: [
         {
             provide: HTTP_INTERCEPTORS,
---
+++ alurapic/src/app/core/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs';
+import { UserService } from './../user/user.service';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user/user';
+
+@Component({
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css'],
+})
+export class FooterComponent implements OnInit {
+    user$!: Observable<User | null>;
+
+    constructor(private userService: UserService) {}
+
+    ngOnInit(): void {
+        this.user$ = this.userService.getUser();
+    }
+}
